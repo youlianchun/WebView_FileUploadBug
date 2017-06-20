2271f7c6002d9494ccba85b28df7c07a33bc2a39
YLCHUN <985050622@qq.com>
2017-06-20 14:26:07 +0800

    图片拦截更新

--- WebView_FileUploadBug/UIViewController+Intercept_FileUploadPane.m
+++ WebView_FileUploadBug/UIViewController+Intercept_FileUploadPane.m
@@ -9,21 +9,56 @@
 #import "UIViewController+Intercept_FileUploadPane.h"
 #import <objc/runtime.h>
 
-@implementation UIViewController (Intercept_FileUploadPane)
-    
-+ (void)load {
-    static dispatch_once_t onceToken;
-    dispatch_once(&onceToken, ^{
-        [self swizzledSelectorWithClassString:@"WKFileUploadPanel"];
-        [self swizzledSelectorWithClassString:@"UIWebFileUploadPanel"];
-    });
+@interface _UIImagePickerController_IFUP : NSObject
+{
+  __weak id<UINavigationControllerDelegate,UIImagePickerControllerDelegate> _receiverDelegate;
+}
+@end
+@implementation _UIImagePickerController_IFUP
+-(instancetype)initWithReceiver:(id<UINavigationControllerDelegate,UIImagePickerControllerDelegate>)receiver {
+    if (self) {
+        _receiverDelegate = receiver;
+    }
+    return self;
+}
+- (void)imagePickerController:(UIImagePickerController *)imagePicker didFinishPickingMediaWithInfo:(NSDictionary *)info {
+    //拦截代码。。。
+    [_receiverDelegate imagePickerController:imagePicker didFinishPickingMediaWithInfo:info];
 }
 
-+(void)swizzledSelectorWithClassString:(NSString*)classString {
-    Class class = NSClassFromString(classString);
+- (id)forwardingTargetForSelector:(SEL)aSelector {
+    id i = [super forwardingTargetForSelector:aSelector];
+    if (!i && [_receiverDelegate respondsToSelector:aSelector]) {
+        i = _receiverDelegate;
+    }
+    return i;
+}
+
+- (BOOL)respondsToSelector:(SEL)aSelector {
+    BOOL b = [super respondsToSelector:aSelector];
+    if (!b) {
+        b = [_receiverDelegate respondsToSelector:aSelector];
+    }
+    return b;
+}
+@end
+
+@interface UIImagePickerController ()
+@property (nonatomic, strong) _UIImagePickerController_IFUP *ifup;
+@end
+@implementation UIImagePickerController (Intercept_FileUploadPane)
+-(void)setIfup:(_UIImagePickerController_IFUP *)ifup {
+    objc_setAssociatedObject(self, @selector(ifup), ifup, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+-(_UIImagePickerController_IFUP *)ifup {
+    return objc_getAssociatedObject(self, @selector(ifup));
+}
+
++(void)load {
+    Class class = [self class];
     if (class) {
-        SEL originalSelector = NSSelectorFromString(@"imagePickerController:didFinishPickingMediaWithInfo:");
-        SEL swizzledSelector = @selector(_imagePickerController:didFinishPickingMediaWithInfo:);
+        SEL originalSelector = @selector(setDelegate:);
+        SEL swizzledSelector = @selector(ifup_setDelegate:);
         Method originalMethod = class_getInstanceMethod(class, originalSelector);
         Method swizzledMethod = class_getInstanceMethod(class, swizzledSelector);
         BOOL success = class_addMethod(class, originalSelector, method_getImplementation(swizzledMethod), method_getTypeEncoding(swizzledMethod));
@@ -32,28 +67,16 @@
         } else {
             method_exchangeImplementations(originalMethod, swizzledMethod);
         }
-        
-        originalSelector = NSSelectorFromString(@"imagePickerController:didFinishPickingMultipleMediaWithInfo:");
-        swizzledSelector = @selector(_imagePickerController:didFinishPickingMultipleMediaWithInfo:);
-        originalMethod = class_getInstanceMethod(class, originalSelector);
-        swizzledMethod = class_getInstanceMethod(class, swizzledSelector);
-        success = class_addMethod(class, originalSelector, method_getImplementation(swizzledMethod), method_getTypeEncoding(swizzledMethod));
-        if (success) {
-            class_replaceMethod(class, swizzledSelector, method_getImplementation(originalMethod), method_getTypeEncoding(originalMethod));
-        } else {
-            method_exchangeImplementations(originalMethod, swizzledMethod);
-        }
     }
 }
 
-- (void)_imagePickerController:(UIImagePickerController *)imagePicker didFinishPickingMediaWithInfo:(NSDictionary *)info {
-    //拦截代码。。。
-    [self _imagePickerController:imagePicker didFinishPickingMediaWithInfo:info];
-}
-
-- (void)_imagePickerController:(UIImagePickerController *)imagePicker didFinishPickingMultipleMediaWithInfo:(NSArray *)infos  {
-    //拦截代码。。。
-    [self _imagePickerController:imagePicker didFinishPickingMultipleMediaWithInfo:infos];
+-(void)ifup_setDelegate:(id<UINavigationControllerDelegate,UIImagePickerControllerDelegate>)delegate {
+    if ([delegate isKindOfClass:NSClassFromString(@"WKFileUploadPanel")]||[delegate isKindOfClass:NSClassFromString(@"UIWebFileUploadPanel")]) {
+        self.ifup = [[_UIImagePickerController_IFUP alloc] initWithReceiver:delegate];
+        [self ifup_setDelegate:(id<UINavigationControllerDelegate,UIImagePickerControllerDelegate>)self.ifup];
+    }else{
+        [self ifup_setDelegate:delegate];
+    }
 }
 
 @end
