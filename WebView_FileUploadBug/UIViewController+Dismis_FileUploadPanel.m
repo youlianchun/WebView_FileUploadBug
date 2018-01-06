232328e7f07dd6b1723a32b57f4f675f376601fd
YLCHUN <ylchun@YLCHUN.local>
2018-01-06 09:56:24 +0800

    bugfix

--- WebView_FileUploadBug/UIViewController+Dismis_FileUploadPanel.m
+++ WebView_FileUploadBug/UIViewController+Dismis_FileUploadPanel.m
@@ -14,48 +14,48 @@
 @end
 @implementation UIViewController (Dismis_FileUploadPanel)
 
--(BOOL)FileUploadPanelFlag {
+-(BOOL)FileUploadPanelFlag
+{
     return [objc_getAssociatedObject(self, @selector(FileUploadPanelFlag)) boolValue];
 }
--(void)setFileUploadPanelFlag:(BOOL)FileUploadPanelFlag {
+
+-(void)setFileUploadPanelFlag:(BOOL)FileUploadPanelFlag
+{
     objc_setAssociatedObject(self, @selector(FileUploadPanelFlag), @(FileUploadPanelFlag), OBJC_ASSOCIATION_RETAIN_NONATOMIC);
 }
 
-+ (void)load {
+static void dfup_swizzledMethod(Class class, SEL originalSelector, SEL swizzledSelector)
+{
+    Method originalMethod = class_getInstanceMethod(class, originalSelector);
+    Method swizzledMethod = class_getInstanceMethod(class, swizzledSelector);
+    BOOL success = class_addMethod(class, originalSelector, method_getImplementation(swizzledMethod), method_getTypeEncoding(swizzledMethod));
+    if (success) {
+        class_replaceMethod(class, swizzledSelector, method_getImplementation(originalMethod), method_getTypeEncoding(originalMethod));
+    } else {
+        method_exchangeImplementations(originalMethod, swizzledMethod);
+    }
+}
+
++ (void)load
+{
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
-        Class class = [self class];
-        SEL originalSelector = @selector(dismissViewControllerAnimated:completion:);
-        SEL swizzledSelector = @selector(dfup_dismissViewControllerAnimated:completion:);
-        Method originalMethod = class_getInstanceMethod(class, originalSelector);
-        Method swizzledMethod = class_getInstanceMethod(class, swizzledSelector);
-        BOOL success = class_addMethod(class, originalSelector, method_getImplementation(swizzledMethod), method_getTypeEncoding(swizzledMethod));
-        if (success) {
-            class_replaceMethod(class, swizzledSelector, method_getImplementation(originalMethod), method_getTypeEncoding(originalMethod));
-        } else {
-            method_exchangeImplementations(originalMethod, swizzledMethod);
-        }
-        
-        originalSelector = @selector(presentViewController:animated:completion:);
-        swizzledSelector = @selector(dfup_presentViewController:animated:completion:);
-        originalMethod = class_getInstanceMethod(class, originalSelector);
-        swizzledMethod = class_getInstanceMethod(class, swizzledSelector);
-        success = class_addMethod(class, originalSelector, method_getImplementation(swizzledMethod), method_getTypeEncoding(swizzledMethod));
-        if (success) {
-            class_replaceMethod(class, swizzledSelector, method_getImplementation(originalMethod), method_getTypeEncoding(originalMethod));
-        } else {
-            method_exchangeImplementations(originalMethod, swizzledMethod);
-        }
+        dfup_swizzledMethod(self, @selector(dismissViewControllerAnimated:completion:), @selector(dfup_dismissViewControllerAnimated:completion:));
+        dfup_swizzledMethod(self, @selector(presentViewController:animated:completion:), @selector(dfup_presentViewController:animated:completion:));
     });
 }
 
 
--(void)dfup_dismissViewControllerAnimated:(BOOL)flag completion:(void (^)(void))completion {
+-(void)dfup_dismissViewControllerAnimated:(BOOL)flag completion:(void (^)(void))completion
+{
     static BOOL dismisFromFileUploadPanel = NO;
-    if (!dismisFromFileUploadPanel) {
+    if (!dismisFromFileUploadPanel)
+    {
         [self dfup_dismissViewControllerAnimated:flag completion:^{
-            if (completion) {
-                if (self.FileUploadPanelFlag) {
+            if (completion)
+            {
+                if (self.FileUploadPanelFlag)
+                {
                     dismisFromFileUploadPanel = YES;
                     completion();
                     dismisFromFileUploadPanel = NO;
@@ -67,15 +67,20 @@
     }
 }
 
--(void)dfup_presentViewController:(UIViewController *)viewControllerToPresent animated:(BOOL)flag completion:(void (^)(void))completion {
-    if ([viewControllerToPresent isKindOfClass:[UIDocumentMenuViewController class]]) {
-        UIDocumentMenuViewController *dvc = (UIDocumentMenuViewController*)viewControllerToPresent;
-        if ([dvc.delegate isKindOfClass:NSClassFromString(@"WKFileUploadPanel")] || [dvc.delegate isKindOfClass:NSClassFromString(@"UIWebFileUploadPanel")]) {
+-(void)dfup_presentViewController:(UIViewController *)viewControllerToPresent animated:(BOOL)flag completion:(void (^)(void))completion
+{
+    //viewControllerToPresent -> UIDocumentMenuViewController UIDocumentPickerViewController
+    if ([viewControllerToPresent respondsToSelector:@selector(delegate)])
+    {
+        id delegate = [viewControllerToPresent valueForKey:@"delegate"];
+        if ([delegate isKindOfClass:objc_getRequiredClass("WKFileUploadPanel")] || [delegate isKindOfClass:objc_getRequiredClass("UIWebFileUploadPanel")])
+        {
             self.FileUploadPanelFlag = YES;
-            dvc.FileUploadPanelFlag = YES;
+            viewControllerToPresent.FileUploadPanelFlag = YES;
         }
     }
     [self dfup_presentViewController:viewControllerToPresent animated:flag completion:completion];
 }
 
 @end
+
